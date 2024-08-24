cf80050022c2e81569ddeea4e9bd433ce24bcdc7
wuzijie747 <122375021+wuzijie747@users.noreply.github.com>
2024-08-24 18:13:05 +0800

    Add files via upload

---
+++ chatify/chatify/ReadWrite.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+namespace chatify
+{
+    internal class ReadWrite
+    {
+        internal static bool Write(string filename, string content)
+        {
+            string path = filename; // 请替换为你的文件路径
+            string tempPath = Path.GetTempFileName(); // 创建临时文件
+            string textToAdd = content; // 你要添加到文件末尾的字符串
+            int maxLines = 100; // 文件最大行数
+            int lineCount = 0; // 当前行数
+            if (!File.Exists(path))
+            {
+                // 文件不存在，创建文件
+                try
+                {
+                    using (FileStream fs = File.Create(path))
+                    {
+                        // 文件已创建，可以在这里写入内容，如果需要的话
+                        // 例如写入一些初始化内容
+                        // byte[] info = new UTF8Encoding(true).GetBytes("This is some text in the file.");
+                        // fs.Write(info, 0, info.Length);
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                using (StreamWriter sw = new StreamWriter(tempPath))
+                {
+                    string line;
+
+                    // 逐行读取原文件
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        // 当行数少于100时，直接写入临时文件
+                        if (lineCount < maxLines)
+                        {
+                            sw.WriteLine(line);
+                            lineCount++;
+                        }
+                        else
+                        {
+                            // 当行数达到100时，开始丢弃前面的行
+                            sw.WriteLine(line);
+                            sw.BaseStream.Seek(0, SeekOrigin.Begin); // 回到文件开头
+                                                                     // 跳过第一行，即最旧的行
+                            sr.ReadLine();
+                        }
+                    }
+
+                    // 添加新行到文件末尾
+                    sw.WriteLine(textToAdd);
+                }
+
+                // 替换原文件
+                File.Delete(path);
+                File.Move(tempPath, path);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred:");
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+    }
+}
---
+++ chatify/chatify/Send.cs
@@ -0,0 +1,77 @@
+﻿using chatify;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+namespace send
+{
+    class Send
+    {
+        internal static bool chat(int port,string ip,UdpClient udpClient,string content,string id)
+        {
+            try
+            {
+                DateTime now = DateTime.Now;
+                string message = "chat:" + id + "," + content + "," + now.ToString("yyyy/MM/dd HH:mm:ss");
+
+                byte[] sendData = Encoding.ASCII.GetBytes(message);
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+                udpClient.Send(sendData, sendData.Length, endPoint);
+                Console.WriteLine("Sent: " + message);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+        internal static bool query(int port, string SERVER_IP, UdpClient udpClient, int EndId)
+        {
+            try
+            {
+                // 发送数据
+                string message = "query:" + EndId.ToString();
+
+
+                byte[] sendData = Encoding.ASCII.GetBytes(message);
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(SERVER_IP), port);
+                udpClient.Send(sendData, sendData.Length, endPoint);
+                Console.WriteLine("Sent: " + message);
+
+                // 接收响应
+                int num;
+                if (EndId <= 15)
+                {
+                    num = EndId;
+                }
+                else
+                {
+                    num = 15;
+                }
+                for (int i = 1; i <= num; i++)
+                {
+                    IPEndPoint receiveEndPoint = new IPEndPoint(IPAddress.Any, port);
+                    byte[] receiveData = udpClient.Receive(ref receiveEndPoint);
+                    string responseMessage = Encoding.ASCII.GetString(receiveData);
+                    ReadWrite.Write("123.txt", responseMessage);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                if (udpClient != null)
+                {
+                    udpClient.Close();
+                }
+            }
+            return true;
+        }
+    }
+}
